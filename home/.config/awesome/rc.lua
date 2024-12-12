1799b419cdfd0a673fc0cf5b7f373b6230e94329
myagko <thonyaerevan@gmail.com>
2024-12-12 20:06:31 +0700

    update

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -2,18 +2,9 @@ pcall(require, "luarocks.loader")
 collectgarbage("setpause", 110)
 collectgarbage("setstepmul", 1000)
 
-local naughty = require("naughty")
-
-naughty.connect_signal("request::display_error", function(message, startup)
-	naughty.notification {
-		urgency = "critical",
-		title = "An error happened" .. (startup and " during startup!" or "!"),
-		message = message
-	}
-end)
-
-require("config.theme")
-require("config.tags")
+require("core.error")
+require("core.theme")
+require("core.tags")
 require("ui")
-require("config.rules")
-require("config.binds")
+require("core.rules")
+require("core.binds")
