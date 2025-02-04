2b862bf0531c83058f25840aba3125ef9cbe5fe4
myagko <thonyaerevan@gmail.com>
2025-02-04 17:18:39 +0700

    update

--- home/.config/awesome/core/theme.lua
+++ home/.config/awesome/core/theme.lua
@@ -10,5 +10,5 @@ local themes = {
 	["nymph"] = true
 }
 
-local theme_name = themes[user.theme] and user.theme or "placid"
+local theme_name = themes[user.theme] and user.theme or "everblush"
 beautiful.init(gfilesystem.get_configuration_dir() .. "themes/" .. theme_name .. "/theme.lua")
--- home/.config/awesome/user.lua
+++ home/.config/awesome/user.lua
@@ -1,7 +1,7 @@
 return {
 	--screenshots_folder = "/path/to/screenshots/folder",
 	terminal = "alacritty",
-	theme = "placid",
+	theme = "haven",
 	--wallpaper = "/path/to/wallpaper",
 	--wallpapers_folder = "/path/to/wallpapers/folder",
 	weather_imperial_units = false,
